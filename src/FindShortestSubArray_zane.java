0d7c5a97f8d6185779bcadbed6aa548fdc88a254
ZhaoZhen <zhaozhen197@gmail.com>
2019-05-07 11:08:01 +0800

    add 2019/05/07 commit algorithms!

--- src/FindShortestSubArray_zane.java
+++ src/FindShortestSubArray_zane.java
@@ -11,6 +11,12 @@ public class FindShortestSubArray_zane {
 
     }
 
+    /**
+     * No.697 数组的度
+     *
+     * @param nums
+     * @return
+     */
     public int findShortestSubArray(int[] nums) {
 
         Map<Integer, Integer> aMap = new HashMap<>();
@@ -78,6 +84,64 @@ public class FindShortestSubArray_zane {
 
         }
         return tempj - tempi + 1;
+    }
+
+    public int findShortestSubArray1(int[] nums) {
+        int[] mark = new int[50000];
+
+        int degree = 0;
+        int minLen = nums.length;
+
+        for (int elem : nums) {
+            mark[elem]++;
+        }
+
+        for (int i = 0; i < mark.length; i++) {
+            if (mark[i] == 0) {
+                continue;
+            }
+            degree = Math.max(degree, mark[i]);
+        }
+        if (degree == 1) {
+            return 1;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int templength;
+
+
+        for (int i = 0; i < mark.length; i++) {
+
+            if (mark[i] == degree) {
+                templength = lengthOfSubarray(nums, i);
+                if (min > templength) {
+                    min = templength;
+                }
+            }
+
+        }
+        return min;
+/*//        System.out.println("最大度为 " + degree);
+
+        // 存放元素之间的距离
+//        Map<Integer, Integer> map1 = new HashMap<>();
+        // 存放元素出现的次数
+        Map<Integer, Integer> map2 = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int count = map2.getOrDefault(nums[i], 0) + 1;
+            if (count == 1){
+                // 首次出现
+                map1.put(nums[i], i);
+            }else if (count == degree){
+                // 最后一次出现
+                int len = i - map1.get(nums[i]) + 1;
+                minLen = Math.min(len, minLen);
+                map1.put(nums[i], minLen);
+            }
+            map2.put(nums[i], count);
+        }
+
+        return minLen;*/
     }
 
 }
